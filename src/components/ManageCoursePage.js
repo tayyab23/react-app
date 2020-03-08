93a30b2ab7e1fbcfb6325451da886caa7855cde5
Tayyab Ahmed <tayyab.ahmed@calabrio.com>
2020-03-08 14:07:04 -0700

    crated a functional form to manage courses

--- src/components/ManageCoursePage.js
+++ src/components/ManageCoursePage.js
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import CourseForm from "./CourseForm";
 
 const ManageCoursePage = props => {
+  const [course, setCourse] = useState({
+    id: null,
+    slug: "",
+    title: "",
+    authorId: "",
+    category: ""
+  });
+
+  function handleChange({ target }) {
+    setCourse({
+      ...course,
+      [target.name]: target.value
+    });
+  }
   return (
     <>
       <h2>Manage Course</h2>
-      {props.match.params.slug}
+      <CourseForm course={course} onChange={handleChange} />
     </>
   );
 };
