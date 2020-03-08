10fe05d5915f6304b5ae027349aee29dc6ecacb9
Tayyab Ahmed <tayyab.ahmed@calabrio.com>
2020-03-08 12:55:39 -0700

    functional base app with a navbar, and a read from db

---
+++ src/components/HomePage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function HomePage() {
+  return (
+    <div className="jumbotron">
+      <h1>Administration</h1>
+          <p>React, Flux, and react Router for responsive web apps</p>
+          <a href="/about">About</a>
+    </div>
+  );
+}
+
+export default HomePage;
