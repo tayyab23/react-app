62945cf0a66cf0cd403b9d4b108467aa0230c190
Tayyab Ahmed <tayyab.ahmed@calabrio.com>
2020-03-08 13:05:11 -0700

    tested the behaviour of Link component where the page is loaded on client site so page nav is instantanious

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,11 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function HomePage() {
   return (
     <div className="jumbotron">
       <h1>Administration</h1>
-          <p>React, Flux, and react Router for responsive web apps</p>
-          <a href="/about">About</a>
+      <p>React, Flux, and react Router for responsive web apps.</p>
+      <Link to="about" className="btn btn-primary">
+        About
+      </Link>
     </div>
   );
 }
